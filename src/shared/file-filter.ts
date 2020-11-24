b11eaf2d828d1531a25d2d56fb7f2db7cfb62b35
ducnghiem2411 <ducnghiem2411@gmail.com>
2020-11-24 09:48:59 +0700

    minor fix file filter

--- src/shared/file-filter.ts
+++ src/shared/file-filter.ts
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus } from '@nestjs/common'
 import { extname } from 'path'
 
 export const imageFilter = function(req, file, cb) {
-  if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|PNG|JPG|JPEG)$/)) {
     cb(new HttpException(`Unsupported file type ${extname(file.originalname)}`, HttpStatus.BAD_REQUEST), false)
   }
   cb(null, true)
