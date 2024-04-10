c6c305dcbd780be9ae78802e115203b1e1f401fc
Albedo <duc.nghiem@blockodyssey.io>
2024-04-10 16:49:32 +0700

    config

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+export const mailer = {
+  service: 'gmail',
+  user: 'tour.book.2024@gmail.com',
+  // user: 'ducnghiem2411@gmail.com',
+  password: 'tywp igrl pdza mxyn'
+};
+
+//List admin accounts (email)
+export const su: Array<string> = [
+  'ducnghiem2411@gmail.com',
+  'tour.book.2024@gmail.com'
+];
